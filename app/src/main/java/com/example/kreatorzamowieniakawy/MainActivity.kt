d529e31b34354e47b1cb3c795466be42c6fb031b
Joseph <blazej.sabisz@cyfroweszkoly.pl>
2025-03-09 20:46:08 +0100

    function SeekBar

--- app/src/main/java/com/example/kreatorzamowieniakawy/MainActivity.kt
+++ app/src/main/java/com/example/kreatorzamowieniakawy/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kreatorzamowieniakawy
 
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val myTextView = findViewById<TextView>(R.id.myTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                //Ta metoda wywoływana jest za każdym razem, gdy zmienia się wartość postępu
+                //fromUser - informacja czy zmiana wywołana przez użytkownika, czy programistycznie
+                myTextView.text = "Ilosc: $progress"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Ta metoda wywoływana jest w momencie, gdy użytkownik dotknie i zacznie przesuwać suwak
+                Toast.makeText(this@MainActivity, "Zacząłeś przesuwać suwak", Toast.LENGTH_SHORT).show()
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                //Ta metoda wywoływana jest w momencie, gdy użytkownik przestanie przesuwać suwak i puści go
+                Toast.makeText(this@MainActivity, "Zakończyłeś przesuwanie suwaka", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
